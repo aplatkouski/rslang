06de8e1a35a38e06dc051d55f3a8c165b2735e36
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-03-24 02:17:26 +0300

    feat: add browser-router, route and switch to app component
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,61 @@
 import { Counter } from 'features/counter/Counter';
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import { ReactComponent as Logo } from './logo.svg';
 
 const App = (): JSX.Element => (
-  <div className="app">
-    <header className="app-header">
-      <Logo className="app-logo" title="logo" />
-      <Counter />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <span>
-        <span>Learn </span>
-        <a
-          className="app-link"
-          href="https://reactjs.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          React
-        </a>
-        <span>, </span>
-        <a
-          className="app-link"
-          href="https://redux.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Redux
-        </a>
-        <span>, </span>
-        <a
-          className="app-link"
-          href="https://redux-toolkit.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Redux Toolkit
-        </a>
-        ,<span> and </span>
-        <a
-          className="app-link"
-          href="https://react-redux.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          React Redux
-        </a>
-      </span>
-    </header>
-  </div>
+  <BrowserRouter basename="/rslang">
+    <div className="app">
+      <header className="app-header">
+        <Logo className="app-logo" title="logo" />
+        <Switch>
+          <Route component={Counter} exact path="/" />
+        </Switch>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="app-link"
+            href="https://reactjs.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="app-link"
+            href="https://redux.js.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="app-link"
+            href="https://redux-toolkit.js.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="app-link"
+            href="https://react-redux.js.org/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
