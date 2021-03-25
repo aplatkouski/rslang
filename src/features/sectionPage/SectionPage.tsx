d3068ba30a2be631a96628d74619fb876f9c2695
natallia-js <fedartsova@tut.by>
2021-03-25 13:58:30 +0300

    feat: add adjacent pages navigation

--- src/features/sectionPage/SectionPage.tsx
+++ src/features/sectionPage/SectionPage.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ListItemText, Typography } from '@material-ui/core';
+import { getAdjacentPages, Page } from 'app/sectors/sectorsSlice';
+import { ArrowBackIos, ArrowForwardIos } from '@material-ui/icons';
+
+import './SectionPage.scss';
 
 type SectionPageParams = {
   sector?: string;
@@ -11,9 +17,29 @@ export default function SectionPage(): JSX.Element {
   const { sector, page, color } = useParams<SectionPageParams>();
   const bgColor: string = color || 'white';
 
+  const adjacentPages: Array<Page | undefined> = useSelector((state: any) =>
+    getAdjacentPages(state, { sectorNum: Number(sector), pageNum: Number(page) })
+  );
+
   return (
     <div style={{ backgroundColor: decodeURIComponent(bgColor) }}>
-      Section Page: {sector} {page}
+      <Typography className="page-header">
+        Раздел {Number(sector) + 1}, страница {page}
+      </Typography>
+      <div className="pages-navigation">
+        {adjacentPages && adjacentPages[0] && (
+          <NavLink className="nav-page" to={adjacentPages[0].url}>
+            <ArrowBackIos />
+            <ListItemText className="page-title" primary={adjacentPages[0].title} />
+          </NavLink>
+        )}
+        {adjacentPages && adjacentPages[1] && (
+          <NavLink className="nav-page" to={adjacentPages[1].url}>
+            <ListItemText className="page-title" primary={adjacentPages[1].title} />
+            <ArrowForwardIos />
+          </NavLink>
+        )}
+      </div>
     </div>
   );
 }
