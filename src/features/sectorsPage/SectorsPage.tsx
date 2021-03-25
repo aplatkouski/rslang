bcb400e90ceaf72203c8474b0ac4a30d72829e7c
natallia-js <fedartsova@tut.by>
2021-03-25 10:15:54 +0300

    feat: add some animation for pages

--- src/features/sectorsPage/SectorsPage.tsx
+++ src/features/sectorsPage/SectorsPage.tsx
@@ -6,13 +6,14 @@ import {
   AccordionDetails,
   ListItemText,
   Box,
-  Paper,
   Typography,
 } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 import { getSectors, Sector } from 'app/sectors/sectorsSlice';
+
+import './SectorsPage.scss';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -44,7 +45,7 @@ export default function SectionsPage(): JSX.Element {
               expandIcon={<ExpandMoreIcon />}
               id="panel1a-header"
             >
-              <Typography className={classes.heading}>{sector.title}</Typography>
+              <Typography className="sector-title">{sector.title}</Typography>
             </AccordionSummary>
             <AccordionDetails>
               <Box
@@ -56,11 +57,13 @@ export default function SectionsPage(): JSX.Element {
               >
                 {sector.pages &&
                   sector.pages.map((page) => (
-                    <Paper key={page.key} className={classes.paper} elevation={3}>
-                      <NavLink to={page.url}>
-                        <ListItemText primary={page.title} style={{ width: 120 }} />
-                      </NavLink>
-                    </Paper>
+                    <div key={page.key} className="box">
+                      <div className="schatten">
+                        <NavLink to={page.url}>
+                          <ListItemText className="page-title" primary={page.title} />
+                        </NavLink>
+                      </div>
+                    </div>
                   ))}
               </Box>
             </AccordionDetails>
