cf79355ffe5d807bb71a9949b896d1e918925d5d
vzabavski <vzabavski123@gmail.com>
2021-03-26 15:24:08 +0300

    fix: resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from 'app/navbar/Navbar';
 import MainPage from 'app/mainPage/MainPage';
 import SectorsPage from 'features/sectorsPage/SectorsPage';
 import SectionPage from 'app/sectionPage/SectionPage';
+import TeamPage from './app/TeamPage/TeamPage';
 
 const App = (): JSX.Element => {
   return (
@@ -13,6 +14,7 @@ const App = (): JSX.Element => {
         <Route component={MainPage} exact path="/" />
         <Route component={SectorsPage} exact path="/sectors" />
         <Route component={SectionPage} exact path="/section/:sector/:page/:color" />
+        <Route component={TeamPage} exact path="/about-team" />
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
