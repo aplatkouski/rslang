f05788b0b6f8ffde01cfdcbcd0ac279cef3fbc49
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-03-28 15:14:06 +0300

    feat: add card buttons (to deleted, to complex)

--- src/features/wordCard/styles.ts
+++ src/features/wordCard/styles.ts
@@ -4,27 +4,21 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 const styles = (theme: Theme) =>
   createStyles({
     root: {
-      padding: theme.spacing(1),
       margin: theme.spacing(2, 1),
       background: theme.palette.background.default,
     },
-    wordImage: {
-      width: theme.spacing(40),
-      hight: theme.spacing(22),
-      margin: theme.spacing(1),
-      borderRadius: '2px',
-      boxShadow: theme.shadows[1],
-      lineHeight: '0',
+    root__complex: {
+      background: 'gray',
     },
     image: {
-      width: '100%',
-      objectFit: 'cover',
-      objectPosition: 'center',
-      borderRadius: 'inherit',
+      width: theme.spacing(40),
+      borderRadius: '2px',
+      boxShadow: theme.shadows[1],
     },
     text: {
       userSelect: 'none',
     },
+    button: {},
   });
 
 export default styles;
