55377c5fdd7347d287a8f0dbdc69db44124bebd1
vzabavski <vzabavski123@gmail.com>
2021-03-29 15:21:15 +0300

    feat: add main page basic layout

--- src/app/mainPage/MainPage.tsx
+++ src/app/mainPage/MainPage.tsx
@@ -1,63 +1,52 @@
-import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { ReactComponent as Logo } from '../../logo.svg';
+import { Container, Grid, Typography } from '@material-ui/core';
+import { withStyles, WithStyles } from '@material-ui/core/styles';
+import React from 'react';
+import styles from './styles';
 
-import './MainPage.scss';
+type Props = WithStyles<typeof styles>;
 
-const Counter = lazy(() => import('features/counter/Counter'));
-
-const MainPage = (): JSX.Element => (
-  <main className="app">
-    <header className="app-header">
-      <Logo className="app-logo" title="logo" />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route component={Counter} exact path="/" />
-        </Switch>
-      </Suspense>
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <span>
-        <span>Learn </span>
-        <a
-          className="app-link"
-          href="https://reactjs.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          React
-        </a>
-        <span>, </span>
-        <a
-          className="app-link"
-          href="https://redux.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Redux
-        </a>
-        <span>, </span>
-        <a
-          className="app-link"
-          href="https://redux-toolkit.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Redux Toolkit
-        </a>
-        ,<span> and </span>
-        <a
-          className="app-link"
-          href="https://react-redux.js.org/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          React Redux
-        </a>
-      </span>
-    </header>
+const MainPage = ({ classes }: Props): JSX.Element => (
+  <main className={classes.app}>
+    <Container className={classes.container}>
+      <Typography variant="h1">
+        RSLang - отличное приложение для изучения английского языка!
+      </Typography>
+    </Container>
+    <Container className={classes.container}>
+      <Grid container spacing={3}>
+        <Grid item sm={6} xs={6}>
+          <div>
+            <p>block</p>
+          </div>
+        </Grid>
+        <Grid item sm={6} xs={6}>
+          <div>
+            <p>block</p>
+          </div>
+        </Grid>
+        <Grid item sm={6} xs={6}>
+          <div>
+            <p>block</p>
+          </div>
+        </Grid>
+        <Grid item sm={6} xs={6}>
+          <div>
+            <p>block</p>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+    <Container className={classes.container}>
+      <div>
+        <p>Video</p>
+      </div>
+    </Container>
+    <Container className={classes.container}>
+      <div>
+        <p>About team</p>
+      </div>
+    </Container>
   </main>
 );
 
-export default MainPage;
+export default withStyles(styles, { withTheme: true })(MainPage);
