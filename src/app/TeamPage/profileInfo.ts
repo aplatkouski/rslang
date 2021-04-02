504ce119e9f8ee3e066a7e96a4d8680d16ddf3e4
natallia-js <fedartsova@tut.by>
2021-04-02 15:37:09 +0300

    refactor: change developer images

--- src/app/TeamPage/profileInfo.ts
+++ src/app/TeamPage/profileInfo.ts
@@ -1,6 +1,9 @@
+// !!! Поскольку для создания проекта был использовн create-react-app, то доступна общая папка
+// (public). Следовательно, пути к изображениям отсчитываются относительно ее.
+
 export const info = [
   {
-    avatar: 'https://cdn.onlinewebfonts.com/svg/img_264570.png',
+    avatar: 'images/man_img1.png',
     link: 'https://github.com/aplatkouski',
     name: 'Artsiom Platkouski',
     description:
@@ -19,7 +22,7 @@ export const info = [
       ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
   },
   {
-    avatar: 'https://cdn.onlinewebfonts.com/svg/img_264570.png',
+    avatar: 'images/woman.png',
     link: 'https://github.com/natallia-js',
     name: 'Natallia Fedartsova',
     description:
@@ -38,7 +41,7 @@ export const info = [
       ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
   },
   {
-    avatar: 'https://cdn.onlinewebfonts.com/svg/img_264570.png',
+    avatar: 'images/man_img2.png',
     link: 'https://github.com/RVitaly197',
     name: 'Vital Raicheu',
     description:
@@ -57,7 +60,7 @@ export const info = [
       ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
   },
   {
-    avatar: 'https://cdn.onlinewebfonts.com/svg/img_264570.png',
+    avatar: 'images/man_img3.png',
     link: 'https://github.com/DmitryBogdan90',
     name: 'DmitryBogdan90',
     description:
@@ -76,7 +79,7 @@ export const info = [
       ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
   },
   {
-    avatar: 'https://cdn.onlinewebfonts.com/svg/img_264570.png',
+    avatar: 'images/man_img4.png',
     link: 'https://github.com/vzabavski',
     name: 'Vitaly Zabavski',
     description:
