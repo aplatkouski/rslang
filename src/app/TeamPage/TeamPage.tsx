abd465e2c4c91a70a1164f964c848276a15ec85f
natallia-js <fedartsova@tut.by>
2021-04-04 15:52:39 +0300

    feat: add contribution text, change team page elements location

--- src/app/TeamPage/TeamPage.tsx
+++ src/app/TeamPage/TeamPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { withStyles, WithStyles } from '@material-ui/core/styles';
-import { Avatar, Container, Divider, Link, Typography } from '@material-ui/core';
+import { Avatar, Container, Divider, Grid, Link, Typography } from '@material-ui/core';
+import GitHubLogo from 'assets/icons/github-logo-dark.svg';
+import clsx from 'clsx';
+import SvgImg from './svgImg/SvgImg';
 import { info } from './profileInfo';
 import styles from './styles';
 
@@ -17,13 +20,21 @@ interface MemberBlockProps {
 const MemberBlock = ({ avatar, description, name, link, classes }: MemberBlockProps) => {
   return (
     <>
-      <div className={classes.item}>
-        <div className={classes.title}>
-          <Avatar className={classes.avatar} src={avatar} />
-          <Link href={link}>{name}</Link>
-        </div>
-        <Typography>{description}</Typography>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <div className={classes.title}>
+            <Avatar className={classes.avatar} src={avatar} />
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className={clsx(classes.contribution, classes.title)}>
+            <Link className={classes.title} href={link}>
+              <SvgImg alt="GitHub logo" src={GitHubLogo} title={link} /> {name}
+            </Link>
+            <Typography>{description}</Typography>
+          </div>
+        </Grid>
+      </Grid>
       <Divider className={classes.divider} variant="middle" />
     </>
   );
