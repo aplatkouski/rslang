a23d27f9e6567d1b56bf0fa8de23d6f0143d99c6
natallia-js <fedartsova@tut.by>
2021-04-04 12:00:26 +0300

    feat: add team members contribution description

--- src/app/TeamPage/profileInfo.ts
+++ src/app/TeamPage/profileInfo.ts
@@ -7,94 +7,38 @@ export const info = [
     link: 'https://github.com/aplatkouski',
     name: 'Artsiom Platkouski',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' +
-      ' incididunt ut labore et dolore magna aliqua. Volutpat consequat mauris nunc' +
-      ' congue. Dignissim sodales ut eu sem integer vitae justo eget. Enim praesent' +
-      'elementum facilisis leo vel. Orci a scelerisque purus semper eget duis at tellus' +
-      ' at. Eget aliquet nibh praesent tristique. Sed faucibus turpis in eu mi bibendum' +
-      ' neque. Adipiscing at in tellus integer feugiat. Egestas dui id ornare arcu ' +
-      ' odio ut. Posuere sollicitudin aliquam ultrices sagittis orci. Massa eget egestas' +
-      ' purus viverra accumsan in. Auctor elit sed vulputate mi sit amet mauris' +
-      ' commodo quis. Est velit egestas dui id ornare arcu odio. Egestas quis ipsum' +
-      ' suspendisse ultrices gravida dictum fusce ut. Ultrices mi tempus imperdiet' +
-      ' nulla malesuada  pellentesque. Vitae elementum curabitur vitae nunc sed velit' +
-      ' dignissim sodales ut. Gravida arcu ac tortor dignissim convallis aenean et' +
-      ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
+      'Team Lead проекта. Отвечал за архитектуру приложения. Координировал работу команды.' +
+      ' Создатель долговременной статистики. Внес вклад в разработку бекенда.',
   },
   {
     avatar: 'images/woman.png',
     link: 'https://github.com/natallia-js',
     name: 'Natallia Fedartsova',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' +
-      ' incididunt ut labore et dolore magna aliqua. Volutpat consequat mauris nunc' +
-      ' congue. Dignissim sodales ut eu sem integer vitae justo eget. Enim praesent' +
-      'elementum facilisis leo vel. Orci a scelerisque purus semper eget duis at tellus' +
-      ' at. Eget aliquet nibh praesent tristique. Sed faucibus turpis in eu mi bibendum' +
-      ' neque. Adipiscing at in tellus integer feugiat. Egestas dui id ornare arcu ' +
-      ' odio ut. Posuere sollicitudin aliquam ultrices sagittis orci. Massa eget egestas' +
-      ' purus viverra accumsan in. Auctor elit sed vulputate mi sit amet mauris' +
-      ' commodo quis. Est velit egestas dui id ornare arcu odio. Egestas quis ipsum' +
-      ' suspendisse ultrices gravida dictum fusce ut. Ultrices mi tempus imperdiet' +
-      ' nulla malesuada  pellentesque. Vitae elementum curabitur vitae nunc sed velit' +
-      ' dignissim sodales ut. Gravida arcu ac tortor dignissim convallis aenean et' +
-      ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
+      'Разработчик. Отвечала за создание дизайна электронного учебника и его страниц, за навигацию' +
+      ' по разделам, страницам учебника, по словарю. Настроила регистрацию и авторизацию пользователя,' +
+      ' роутинг. Внесла вклад в разработку бекенда и страницы "О команде".',
   },
   {
     avatar: 'images/man_img2.png',
     link: 'https://github.com/RVitaly197',
     name: 'Vital Raicheu',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' +
-      ' incididunt ut labore et dolore magna aliqua. Volutpat consequat mauris nunc' +
-      ' congue. Dignissim sodales ut eu sem integer vitae justo eget. Enim praesent' +
-      'elementum facilisis leo vel. Orci a scelerisque purus semper eget duis at tellus' +
-      ' at. Eget aliquet nibh praesent tristique. Sed faucibus turpis in eu mi bibendum' +
-      ' neque. Adipiscing at in tellus integer feugiat. Egestas dui id ornare arcu ' +
-      ' odio ut. Posuere sollicitudin aliquam ultrices sagittis orci. Massa eget egestas' +
-      ' purus viverra accumsan in. Auctor elit sed vulputate mi sit amet mauris' +
-      ' commodo quis. Est velit egestas dui id ornare arcu odio. Egestas quis ipsum' +
-      ' suspendisse ultrices gravida dictum fusce ut. Ultrices mi tempus imperdiet' +
-      ' nulla malesuada  pellentesque. Vitae elementum curabitur vitae nunc sed velit' +
-      ' dignissim sodales ut. Gravida arcu ac tortor dignissim convallis aenean et' +
-      ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
+      'Разработчик. Создатель "умных" карточек слов, которые могут менять свой внешний вид и' +
+      ' поведение в зависимости от того, на какую страницу учебника попадут.',
   },
   {
     avatar: 'images/man_img3.png',
     link: 'https://github.com/DmitryBogdan90',
     name: 'DmitryBogdan90',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' +
-      ' incididunt ut labore et dolore magna aliqua. Volutpat consequat mauris nunc' +
-      ' congue. Dignissim sodales ut eu sem integer vitae justo eget. Enim praesent' +
-      'elementum facilisis leo vel. Orci a scelerisque purus semper eget duis at tellus' +
-      ' at. Eget aliquet nibh praesent tristique. Sed faucibus turpis in eu mi bibendum' +
-      ' neque. Adipiscing at in tellus integer feugiat. Egestas dui id ornare arcu ' +
-      ' odio ut. Posuere sollicitudin aliquam ultrices sagittis orci. Massa eget egestas' +
-      ' purus viverra accumsan in. Auctor elit sed vulputate mi sit amet mauris' +
-      ' commodo quis. Est velit egestas dui id ornare arcu odio. Egestas quis ipsum' +
-      ' suspendisse ultrices gravida dictum fusce ut. Ultrices mi tempus imperdiet' +
-      ' nulla malesuada  pellentesque. Vitae elementum curabitur vitae nunc sed velit' +
-      ' dignissim sodales ut. Gravida arcu ac tortor dignissim convallis aenean et' +
-      ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
+    description: 'Разработчик. Создатель мини-игр.',
   },
   {
     avatar: 'images/man_img4.png',
     link: 'https://github.com/vzabavski',
     name: 'Vitaly Zabavski',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor' +
-      ' incididunt ut labore et dolore magna aliqua. Volutpat consequat mauris nunc' +
-      ' congue. Dignissim sodales ut eu sem integer vitae justo eget. Enim praesent' +
-      'elementum facilisis leo vel. Orci a scelerisque purus semper eget duis at tellus' +
-      ' at. Eget aliquet nibh praesent tristique. Sed faucibus turpis in eu mi bibendum' +
-      ' neque. Adipiscing at in tellus integer feugiat. Egestas dui id ornare arcu ' +
-      ' odio ut. Posuere sollicitudin aliquam ultrices sagittis orci. Massa eget egestas' +
-      ' purus viverra accumsan in. Auctor elit sed vulputate mi sit amet mauris' +
-      ' commodo quis. Est velit egestas dui id ornare arcu odio. Egestas quis ipsum' +
-      ' suspendisse ultrices gravida dictum fusce ut. Ultrices mi tempus imperdiet' +
-      ' nulla malesuada  pellentesque. Vitae elementum curabitur vitae nunc sed velit' +
-      ' dignissim sodales ut. Gravida arcu ac tortor dignissim convallis aenean et' +
-      ' tortor at. Amet dictum sit amet justo donec. Magna etiam tempor orci eu.',
+      'Разработчик. Создатель главной страницы приложения, страницы "О команде". Отвечал за' +
+      ' пользовательские настройки приложения и кратковременную статистику.',
   },
 ];
