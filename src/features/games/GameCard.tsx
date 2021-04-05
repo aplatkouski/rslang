194e78520f692609b024eb528d80b075ffceeebb
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-04-05 22:28:18 +0300

    refactor: rearrange imports in GameCard.tsx
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/features/games/GameCard.tsx
+++ src/features/games/GameCard.tsx
@@ -1,8 +1,5 @@
-import { WithStyles, withStyles } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import { WithStyles, withStyles } from '@material-ui/core/styles';
 import { useImg } from 'common/hooks';
 import React from 'react';
 import { IGame } from 'types';
