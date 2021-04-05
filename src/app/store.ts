589239b958c6eace179c762b3269039a796c7549
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-04-05 11:50:46 +0300

    fix: remove redundant 'counter' feature
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,4 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
-import counterReducer from 'features/counter/counterSlice';
 import gameStatisticsReducer from 'features/game-statistics/gameStatisticsSlice';
 import gamesReducer from 'features/games/gamesSlice';
 import sectorsReducer from 'features/sectors/sectorsSlice';
@@ -10,7 +9,6 @@ import wordsReducer from 'features/words/wordsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     gameStatistics: gameStatisticsReducer,
     games: gamesReducer,
     sectors: sectorsReducer,
