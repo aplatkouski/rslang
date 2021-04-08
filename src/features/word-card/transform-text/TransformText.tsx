719cd312ae29dda18680a9ff420a83e8705f9a1d
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-04-08 03:51:19 +0300

    fix: add 'memo' to components
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/features/word-card/transform-text/TransformText.tsx
+++ src/features/word-card/transform-text/TransformText.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 interface IProps {
   text: string;
@@ -26,4 +26,4 @@ const TransformText = ({ text }: IProps) => {
   return <>{text}</>;
 };
 
-export default TransformText;
+export default memo(TransformText);
