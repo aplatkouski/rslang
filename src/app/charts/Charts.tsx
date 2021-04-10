4d94bb268e4e00290b03390fec3f01f67c04c257
vzabavski <vzabavski123@gmail.com>
2021-04-10 18:07:14 +0300

    feat: add second chart

--- src/app/charts/Charts.tsx
+++ src/app/charts/Charts.tsx
@@ -5,6 +5,7 @@ import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts'
 import { useAppSelector } from '../../common/hooks';
 import { getCurrUser } from '../../features/user/userSlice';
 import { selectWordCountByDate } from '../../features/word-statistics/wordStatisticsSlice';
+import { aggregated } from './response';
 import styles from './styles';
 
 type Props = WithStyles<typeof styles>;
@@ -29,29 +30,63 @@ const Charts = ({ classes }: Props): JSX.Element => {
         words: wordCount,
       })
     );
-    return result;
+    return result.reverse();
   };
   const wordsPerDayArray = parseToValidData(stats);
+  console.log(wordsPerDayArray);
+  const aggregateTotalStatistics = (array: any) => {
+    const reversedArray = array.reverse();
+    const result: any = [];
+    reversedArray.forEach((item: any, idx: number, arr: any) => {
+      const totalWords = arr
+        .slice(idx)
+        .reduce((acc: number, cur: any) => acc + cur.words, 0);
+      result.push({
+        studiedAt: item.studiedAt,
+        words: totalWords,
+      });
+    });
+    return result.reverse();
+  };
+  const totalWordsArray = aggregateTotalStatistics(aggregated);
   if (!user.userId) {
     return (
       <Typography variant="subtitle1">Авторизуйтесь, чтобы получать данные</Typography>
     );
   }
   return (
-    <div className={classes.chart}>
-      {wordsPerDayArray.length ? (
-        <LineChart
-          data={wordsPerDayArray}
-          height={400}
-          margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
-          width={400}
-        >
-          <Tooltip />
-          <CartesianGrid stroke="#f5f5f5" />
-          <Line dataKey="words" stroke="#ff7300" type="monotone" yAxisId={0} />
-          <XAxis dataKey="studiedAt" />
-          <YAxis />
-        </LineChart>
+    <div>
+      {wordsPerDayArray ? (
+        <>
+          <div className={classes.chart}>
+            <LineChart
+              data={aggregated.reverse()}
+              height={400}
+              margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
+              width={400}
+            >
+              <Tooltip />
+              <CartesianGrid stroke="#f5f5f5" />
+              <Line dataKey="words" stroke="#ff7300" type="monotone" yAxisId={0} />
+              <XAxis dataKey="studiedAt" />
+              <YAxis />
+            </LineChart>
+          </div>
+          <div className={classes.chart}>
+            <LineChart
+              data={totalWordsArray}
+              height={400}
+              margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
+              width={400}
+            >
+              <Tooltip />
+              <CartesianGrid stroke="#f5f5f5" />
+              <Line dataKey="words" stroke="#ff7300" type="monotone" yAxisId={0} />
+              <XAxis dataKey="studiedAt" />
+              <YAxis />
+            </LineChart>
+          </div>
+        </>
       ) : (
         <Typography variant="h5">Пока нет данных</Typography>
       )}
