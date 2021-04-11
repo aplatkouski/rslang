75e725d45c5919e1248bd25eecb9e215676c9e94
vzabavski <vzabavski123@gmail.com>
2021-04-11 14:35:49 +0300

    refactor: remove aggregateDailyStatistics

--- src/app/charts/Charts.tsx
+++ src/app/charts/Charts.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { useAppSelector } from '../../common/hooks';
-import { selectDifficultUserWordsCountByDate } from '../../features/user-words/userWordsSlice';
-import { getCurrUser } from '../../features/user/userSlice';
-import { selectWordCountByDate } from '../../features/word-statistics/wordStatisticsSlice';
+import { selectStudiedUserWordsCountByDate } from '../../features/user-words/userWordsSlice';
+import { selectCredentials } from '../../features/user/userSlice';
 import styles from './styles';
 
 type Props = WithStyles<typeof styles>;
@@ -20,9 +19,8 @@ interface ChartData {
 }
 
 const Charts = ({ classes }: Props): JSX.Element => {
-  const user = useAppSelector(getCurrUser);
-  const stats = useAppSelector(selectWordCountByDate);
-  const studied = useAppSelector(selectDifficultUserWordsCountByDate);
+  const user = useAppSelector(selectCredentials);
+  const studied = useAppSelector(selectStudiedUserWordsCountByDate);
   const parseToValidData = (data: Report) => {
     const result: Array<ChartData> = [];
     Object.entries(data).map(([studiedAt, wordCount]) =>
@@ -31,27 +29,11 @@ const Charts = ({ classes }: Props): JSX.Element => {
         words: wordCount,
       })
     );
-    return result;
-  };
-  const aggregateDailyStatistics = () => {
-    const iterable = [...parseToValidData(stats), ...parseToValidData(studied)];
-    const result: Array<ChartData> = [];
-    const uniqueDate = new Set();
-    iterable.forEach((item) => uniqueDate.add(item.studiedAt));
-    uniqueDate.forEach((date) => {
-      const aggregated = iterable
-        .filter((item) => item.studiedAt === date)
-        .reduce((acc, cur) => acc + cur.words, 0);
-      result.push({
-        studiedAt: String(date),
-        words: aggregated,
-      });
-    });
     return result.sort(
       (prev, next) => Number(prev.studiedAt.slice(-2)) - Number(next.studiedAt.slice(-2))
     );
   };
-  const wordsPerDayArray = aggregateDailyStatistics();
+  const wordsPerDayArray = parseToValidData(studied);
   const aggregateTotalStatistics = (array: Array<ChartData>) => {
     const reversedArray = array.reverse();
     const result: Array<ChartData> = [];
@@ -67,9 +49,11 @@ const Charts = ({ classes }: Props): JSX.Element => {
     return result.reverse();
   };
   const totalWordsArray = aggregateTotalStatistics(wordsPerDayArray);
-  if (!user.userId) {
+  if (!user) {
     return (
-      <Typography variant="subtitle1">Авторизуйтесь, чтобы получать данные</Typography>
+      <Typography className={classes.chartTitle} variant="subtitle1">
+        Авторизуйтесь, чтобы получать данные
+      </Typography>
     );
   }
   return (
@@ -84,7 +68,7 @@ const Charts = ({ classes }: Props): JSX.Element => {
               data={wordsPerDayArray.reverse()}
               height={400}
               margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
-              width={400}
+              width={480}
             >
               <Tooltip />
               <CartesianGrid stroke="#f5f5f5" />
@@ -95,13 +79,13 @@ const Charts = ({ classes }: Props): JSX.Element => {
           </div>
           <div className={classes.chart}>
             <Typography className={classes.chartTitle} variant="subtitle1">
-              Общее количество слов за каждый день
+              Общее число изученных слов за каждый день
             </Typography>
             <LineChart
               data={totalWordsArray}
               height={400}
               margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
-              width={400}
+              width={480}
             >
               <Tooltip />
               <CartesianGrid stroke="#f5f5f5" />
--- src/app/charts/styles.ts
+++ src/app/charts/styles.ts
@@ -3,7 +3,7 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 const styles = (theme: Theme) =>
   createStyles({
     chart: {
-      minWidth: theme.spacing(62.5),
+      minWidth: theme.spacing(63),
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
