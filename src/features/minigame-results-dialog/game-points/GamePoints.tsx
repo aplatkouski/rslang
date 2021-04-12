81cb24eb3962030989cc18e4d799d8f5e4c3851e
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-04-12 13:22:33 +0300

    feat: add select bestSeries by gameId to GamePoint

--- src/features/minigame-results-dialog/game-points/GamePoints.tsx
+++ src/features/minigame-results-dialog/game-points/GamePoints.tsx
@@ -1,5 +1,7 @@
 import { Box, Typography, WithStyles, withStyles } from '@material-ui/core';
 import React from 'react';
+import { selectBestSeriesByGame } from 'features/game-statistics/gameStatisticsSlice';
+import { useAppSelector } from '../../../common/hooks';
 import { GameResult } from '../types';
 import styles from './styles';
 
@@ -14,6 +16,9 @@ interface Props extends WithStyles<typeof styles> {
 
 const GamePoints: React.FC<Props> = ({ classes, data }) => {
   const { gameId, points, bestSeries } = data;
+
+  const bestSeriesGame = useAppSelector((state) => selectBestSeriesByGame(state, gameId));
+
   const average = gameId;
   const record = gameId;
 
@@ -29,15 +34,21 @@ const GamePoints: React.FC<Props> = ({ classes, data }) => {
       <Typography gutterBottom>{TITLE_TEXT}</Typography>
       <Typography>
         {THIS_GAME_TEXT}
-        <span className={classes.result}>{`${points} / ${bestSeries}`}</span>
+        <span className={classes.result}>
+          {bestSeries ? `${points} / ${bestSeries}` : points}
+        </span>
       </Typography>
       <Typography>
         {AVERAGE_PER_GAME_TEXT}
-        <span className={classes.result}>{`${average} / ${average}`}</span>
+        <span className={classes.result}>
+          {bestSeries ? `${average} / ${average}` : average}
+        </span>
       </Typography>
       <Typography>
         {RECORD_PER_GAME_TEXT}
-        <span className={classes.result}>{`${record} / ${record}`}</span>
+        <span className={classes.result}>
+          {bestSeries ? `${record} / ${bestSeriesGame?.bestSeries}` : record}
+        </span>
       </Typography>
     </Box>
   );
