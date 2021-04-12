53bf8e7f19256238c961d28fe366529d18bfeff2
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-04-12 15:28:54 +0300

    feat: add learning progress bar and high-priority mark to word card
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/features/word-card/learning-progress/LearningProgress.tsx
+++ src/features/word-card/learning-progress/LearningProgress.tsx
@@ -1,22 +1,37 @@
-import { LinearProgress, WithStyles } from '@material-ui/core';
+import { LinearProgress, Tooltip, WithStyles } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import React, { memo } from 'react';
+import { useAppSelector } from '../../../common/hooks';
+import { selectCorrectVsWrongByWordId } from '../../word-statistics/wordStatisticsSlice';
 import styles from './styles';
 
 interface IProps extends WithStyles<typeof styles> {
-  value: number;
+  wordId: string;
 }
-const LearningProgress = ({ classes, value }: IProps) => (
-  <LinearProgress
-    classes={{
-      determinate: classes.determinate,
-      bar: classes.bar,
-      colorPrimary: classes.colorPrimary,
-    }}
-    color="primary"
-    value={value}
-    variant="determinate"
-  />
-);
+const LearningProgress = ({ classes, wordId }: IProps) => {
+  const {
+    correctAnswerTotal: correct,
+    wrongAnswerTotal: wrong,
+  } = useAppSelector((state) => selectCorrectVsWrongByWordId(state, { wordId }));
+  const value = correct + wrong ? Math.round((correct / (correct + wrong)) * 100) : 0;
+  return (
+    <Tooltip
+      aria-label="learning progress"
+      arrow
+      title={correct + wrong ? `${correct} vs ${wrong}` : ''}
+    >
+      <LinearProgress
+        classes={{
+          determinate: classes.determinate,
+          bar: classes.bar,
+          colorPrimary: classes.colorPrimary,
+        }}
+        color="primary"
+        value={value}
+        variant="determinate"
+      />
+    </Tooltip>
+  );
+};
 
 export default withStyles(styles, { withTheme: true })(memo(LearningProgress));
