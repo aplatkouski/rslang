1b97af1ec56606d4de2e8318e8044733dcb65c36
natallia-js <fedartsova@tut.by>
2021-04-13 21:35:00 +0300

    feat: move additional my game files in project directories

---
+++ src/features/games/my-game/shuffleAlgorithm.ts
@@ -0,0 +1,22 @@
+/**
+ * Случайная перестановка заданных чисел
+ */
+export function shuffle(array: Array<number>) {
+  let i = array.length;
+  let j = 0;
+  let temp;
+
+  // eslint-disable-next-line no-plusplus
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+
+    // swap randomly chosen element with current element
+    temp = array[i];
+    // eslint-disable-next-line no-param-reassign
+    array[i] = array[j];
+    // eslint-disable-next-line no-param-reassign
+    array[j] = temp;
+  }
+
+  return array;
+}
