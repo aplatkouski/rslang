b1f7411b9877bdf8c6b4b4b1942ebe8f3038d5c5
natallia-js <fedartsova@tut.by>
2021-04-17 19:39:20 +0300

    refactor: restore sectors page styles

--- src/app/groups-page/styles.ts
+++ src/app/groups-page/styles.ts
@@ -2,7 +2,11 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 
 const styles = (theme: Theme) =>
   createStyles({
-    sectorTitle: {
+    root: {
+      width: '100%',
+      height: '100%',
+    },
+    groupTitle: {
       fontSize: theme.typography.pxToRem(17),
       fontWeight: theme.typography.fontWeightBold,
     },
